2380ec385678d4dd8fc98fa2fdaaff8bb664693c
cardi <graytraces@gmail.com>
2019-03-02 12:16:10 +0900

    inflearn lecture practice

---
+++ src/test/java/com/cardi/demoinflearnrestapi/DemoInflearnRestApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cardi.demoinflearnrestapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoInflearnRestApiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
