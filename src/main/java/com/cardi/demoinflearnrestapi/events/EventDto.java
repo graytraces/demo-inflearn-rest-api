1ac872cb87146483a11ae7930d60402a3fa2fdd5
cardi <graytraces@gmail.com>
2019-03-02 23:31:32 +0900

    Add error config for bad parameters.
    - Server will answer "BAD Request(400)" when request parameters doesn't
    match on controller parameters.

--- src/main/java/com/cardi/demoinflearnrestapi/events/EventDto.java
+++ src/main/java/com/cardi/demoinflearnrestapi/events/EventDto.java
@@ -1,12 +1,14 @@
 package com.cardi.demoinflearnrestapi.events;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data @NoArgsConstructor @AllArgsConstructor
+@Builder
 public class EventDto {
     private String name;
     private String description;
--- src/test/java/com/cardi/demoinflearnrestapi/events/EventControllerTests.java
+++ src/test/java/com/cardi/demoinflearnrestapi/events/EventControllerTests.java
@@ -32,6 +32,35 @@ public class EventControllerTests {
 
     @Test
     public void createEvent() throws Exception {
+        EventDto event = EventDto.builder()
+                .name("Spring")
+                .description("Rest Api Development With Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2018, 11, 22, 11, 22, 33))
+                .closeEnrollmentDateTime(LocalDateTime.of(2018, 11, 30, 11, 22, 33))
+                .beginEventDateTime(LocalDateTime.of(2018, 11, 22, 11, 22, 33))
+                .endEventDateTime(LocalDateTime.of(2018, 11, 22, 11, 22, 33))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역 D2 스타트업 팩토리")
+                .build();
+
+        mockMvc.perform( post("/api/events/")
+                    .contentType((MediaType.APPLICATION_JSON_UTF8))
+                    .accept(MediaTypes.HAL_JSON)
+                    .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists())
+                .andExpect(header().exists(HttpHeaders.LOCATION))
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_UTF8_VALUE))
+                .andExpect(jsonPath("id").value(Matchers.not(100)))
+                .andExpect(jsonPath("free").value(Matchers.not(true)))
+                .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()));
+    }
+
+    @Test
+    public void createEvent_BadRequest() throws Exception {
         Event event = Event.builder()
                 .id(100)
                 .name("Spring")
@@ -49,18 +78,10 @@ public class EventControllerTests {
                 .build();
 
         mockMvc.perform( post("/api/events/")
-                    .contentType((MediaType.APPLICATION_JSON_UTF8))
-                    .accept(MediaTypes.HAL_JSON)
-                    .content(objectMapper.writeValueAsString(event)))
+                .contentType((MediaType.APPLICATION_JSON_UTF8))
+                .accept(MediaTypes.HAL_JSON)
+                .content(objectMapper.writeValueAsString(event)))
                 .andDo(print())
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("id").exists())
-                .andExpect(header().exists(HttpHeaders.LOCATION))
-                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_UTF8_VALUE))
-                .andExpect(jsonPath("id").value(Matchers.not(100)))
-                .andExpect(jsonPath("free").value(Matchers.not(true)))
-                .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()))
-        ;
-
+                .andExpect(status().isBadRequest());
     }
 }
