38f925479c844705efaea71ad5147fd29443b401
cardi <graytraces@gmail.com>
2019-03-03 13:37:40 +0900

    shorten "EventResource" to use "Resource"
    - Convert from "ResourceSupport" to "Resource"

--- src/main/java/com/cardi/demoinflearnrestapi/events/EventResource.java
+++ src/main/java/com/cardi/demoinflearnrestapi/events/EventResource.java
@@ -1,18 +1,29 @@
 package com.cardi.demoinflearnrestapi.events;
 
 import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.ResourceSupport;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 
-public class EventResource extends ResourceSupport {
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 
-    @JsonUnwrapped
-    private Event event;
+public class EventResource extends Resource<Event> {
 
-    public EventResource(Event event) {
-        this.event = event;
+    public EventResource(Event event, Link... links) {
+        super(event, links);
+        add(linkTo(EventController.class).slash(event.getId()).withSelfRel());
     }
 
-    public Event getEvent() {
-        return event;
+    public void addQueryEventsLink(){
+        add(linkTo(EventController.class).withRel("query-events"));
+
     }
+
+    public void addUpdateEventsLink(ControllerLinkBuilder selfLinkBuilder){
+        add(selfLinkBuilder.withRel("update-event"));
+    }
+
+
+
 }
