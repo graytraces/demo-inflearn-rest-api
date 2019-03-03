1c60b3069811976551cf331adc7141cb84129129
cardi <graytraces@gmail.com>
2019-03-03 10:35:56 +0900

    Add error info in body when errors is occured.

---
+++ src/main/java/com/cardi/demoinflearnrestapi/common/ErrorSerializer.java
@@ -0,0 +1,49 @@
+package com.cardi.demoinflearnrestapi.common;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import org.springframework.boot.jackson.JsonComponent;
+import org.springframework.validation.Errors;
+
+import java.io.IOException;
+
+@JsonComponent
+public class ErrorSerializer extends JsonSerializer<Errors> {
+    @Override
+    public void serialize(Errors errors, JsonGenerator gen, SerializerProvider serializerProvider) throws IOException {
+        //error.rejectValue()는 Field Error
+        //error.reject() 글로벌에러
+
+        gen.writeStartArray();
+        errors.getFieldErrors().forEach(e->{
+            try {
+                gen.writeStartObject();
+                gen.writeStringField("field", e.getField());
+                gen.writeStringField("objectName", e.getObjectName());
+                gen.writeStringField("code", e.getCode());
+                gen.writeStringField("defaultMessage", e.getDefaultMessage());
+                Object rejectedValue = e.getRejectedValue();
+                if(rejectedValue != null){
+                    gen.writeStringField("rejectedValue", rejectedValue.toString());
+                }
+                gen.writeEndObject();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        });
+
+        errors.getGlobalErrors().forEach(e->{
+            try {
+                gen.writeStartArray();
+                gen.writeStringField("objectName", e.getObjectName());
+                gen.writeStringField("code", e.getCode());
+                gen.writeStringField("defaultMessage", e.getDefaultMessage());
+                gen.writeEndArray();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        });
+        gen.writeEndArray();
+    }
+}
--- src/main/java/com/cardi/demoinflearnrestapi/events/EventController.java
+++ src/main/java/com/cardi/demoinflearnrestapi/events/EventController.java
@@ -33,11 +33,11 @@ public class EventController {
     @PostMapping
     public ResponseEntity createEvent(@RequestBody @Valid EventDto eventDto, Errors errors){
         if(errors.hasErrors()){
-            return ResponseEntity.badRequest().build();
+            return ResponseEntity.badRequest().body(errors);
         }
         eventValidator.validte(eventDto, errors);
         if(errors.hasErrors()){
-            return ResponseEntity.badRequest().build();
+            return ResponseEntity.badRequest().body(errors);
         }
 
         Event event = modelMapper.map(eventDto,Event.class);
